267270f6e2d7826999e274843d4d829df272a35f
Isaac Sukin <ice@icecreamyou.com>
2012-01-13 23:31:19 -0500

    Intermediate changes

--- main.js
+++ main.js
@@ -1,18 +1,89 @@
+var page = 0, money = 10000;
+var stocks = {
+		'BEER': new stock('BEER', 20),
+		'BLUE': new stock('BLUE', 20),
+		'BUBL': new stock('BUBL', 20),
+		'DRUG': new stock('DRUG', 20),
+		'LOVE': new stock('LOVE', 20),
+};
+
 $(document).ready(function() {
+	$('td.cash + td').html(money);
+	
 	var submitButton = null;
 	$('#controls').submit(function(e) {
+		if (submitButton == 'buy') {
+			buy();
+		}
+		else if (submitButton == 'sell') {
+			sell();
+		}
 		e.preventDefault();
 	}).on('keyup mouseup', function(e) {
 		submitButton = e.target.name;
+	});
+	
+	$('#continue').submit(function(e) {
+		page++;
+		if (page == 3) {
+			page = 'congrats';
+		}
+		else if (page == 'congrats') {
+			return false; // TODO exit training
+		}
+		$.get('training_'+ page +'.html', function(data) {
+			$('#content').html($(data).find('#content'));
+		});
+		e.preventDefault();
 	});
 });
 
 // STOCKS ---------------------------------------------------------------------
 
-var stock = function() {
+var stock = function(name, value) {
 	this.amount = 0;
-	this.value = 10;
-	this.pastValues = [];
+	this.name = name;
+	this.value = value;
+	this.allvalues = [value];
+	this.changeValue = function(newValue) {
+		this.allValues.push(newValue);
+		value = newValue;
+	}
+	this.buy = function(amount) {
+		var cost = amount * this.value;
+		if (cost <= money) {
+			money -= cost;
+			this.amount += amount;
+			$('td.cash + td').html(money);
+			// TODO: update display of amounts
+			return true;
+		}
+		return Math.floor(money / this.value);
+	}
+	this.sell = function(amount) {
+		if (amount <= this.amount) {
+			this.amount -= amount;
+			money += amount * this.value;
+			$('td.cash + td').html(money);
+			// TODO: update display of amounts
+			return true;
+		}
+		return this.amount;
+	}
+}
+
+function buy() {
+}
+
+function sell() {
+}
+
+function drawAmounts() {
+	var $tbody = $('#stocks tbody');
+	$tbody.empty();
+	for (name in stocks) {
+		$body.append();// TODO HERE
+	}
 }
 
 
