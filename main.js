f2f109b4e753259e3cd3cf3fb6dcaeac569cfd7d
Casey Rosengren <caser@wharton.upenn.edu>
2012-01-14 21:50:41 -0500

    added function resetChange() to reset the dollarChange on screens where there are no changes in stock

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ $(document).ready(function() {
     $('#continue').submit(function(e) {
         e.preventDefault();
         if (page == 'congrats') {
+        	resetChange();
             page = 'game';
             $.get('index.html', function(data) {
                 $('#content').html($(data).find('#content'));
@@ -68,6 +69,7 @@ $(document).ready(function() {
         else if (page == 'seriousBusiness') {
             $('body').addClass('black');
             seriousBusiness();
+            resetChange();
             return false;
         }
         else {
@@ -86,6 +88,7 @@ $(document).ready(function() {
                     $('#stock option[value="BEER"]').after('<option value="BUBL">BUBL</option>');
                 }
                 else if (page == 'congrats') {
+                	resetChange();
                     stocks.BUBL.changeValue(stocks.BUBL.value + 1);
                     $('#stock option[value="BEER"]').after('<option value="BLUE">BLUE</option>');
                     $('#stock option[value="BUBL"]').after('<option value="DRUG">DRUG</option>');
@@ -208,6 +211,15 @@ function drawAmounts() {
         }
       });
     });
+}
+
+function resetChange() {
+	 for (stock in stocks) {
+	        if (stocks.hasOwnProperty(stock)) {
+	            var stockValue = stocks[stock].value;
+	            stocks[stock].allValues.push(stockValue);
+	        }
+	 }
 }
 
 
